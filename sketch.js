e344f1a3e73827b710cbe71c4aac32a504680b2f
yogeshDS1 <98090498+yogeshDS1@users.noreply.github.com>
2022-03-03 16:42:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var sea,ship;
+var seaImg,shipImg;
+
+function preload(){
+  seaImg=loadImage("sea.png")
+shipImg=loadImage("ship-1.png")
+
+}
+
+function setup(){
+  //moving background
+  createCanvas(400,400);
+  background("blue");
+  sea=createSprite(400,200);
+  sea.addImage(seaImg);
+  sea.scale = 0.3;
+
+  ship= createSprite(130,200,30,30);
+  ship.addImage(shipImg);
+  ship.scale=0.25;
+}
+
+
+
+
+
+function draw() {
+  background(0);
+ drawSprites();
+}
